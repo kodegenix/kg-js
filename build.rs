a33e058664a1d375fa633f4009f73ba8c380237e
jchlapinski <jakub.chlapinski@kodegenix.pl>
2019-09-10 15:05:58 +0200

    fix in build.rs

--- build.rs
+++ build.rs
@@ -1,19 +1,25 @@
 use std::path::PathBuf;
 
+const DUKTAPE_SRC: &str = "lib/duktape";
+
 fn main() {
-    let mut files: Vec<PathBuf> = std::fs::read_dir("lib/duktape")
+    let mut files: Vec<PathBuf> = std::fs::read_dir(DUKTAPE_SRC)
         .unwrap()
         .map(|e| e.unwrap().path())
-        .filter(|p| match p.extension() {
-            Some(s) => s == "c",
-            None => false
-        })
+        .filter(|p| p.extension().map_or(false, |s| s == "c" || s == "h"))
         .collect();
 
     files.sort();
 
+    println!("cargo:rerun-if-changed={}", DUKTAPE_SRC);
+    for p in files.iter() {
+        println!("cargo:rerun-if-changed={}", p.display());
+    }
+
     cc::Build::new()
-        .files(files)
+        .files(files
+            .into_iter()
+            .filter(|p| p.extension().map_or(false, |s| s == "c")))
         .flag_if_supported("-Wimplicit-fallthrough=2")
         .compile("libduktape.a");
 }
